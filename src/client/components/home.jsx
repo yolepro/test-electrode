effb2907cceb149c11dba070b995dd291272cc2b
user <user@MacBook-Pro-de-user.local>
2017-12-01 23:12:29 +0100

    updated component home.jsx

--- src/client/components/home.jsx
+++ src/client/components/home.jsx
@@ -35,7 +35,7 @@ export default () =>
 
     <section className={custom.header}>
       <h2 className={skeleton.title}>
-        Hey this is a nice course! {' '}
+        Hey this is a nice course! 2 {' '}
         <a href="https://github.com/electrode-io">{'Electrode'} <img src={electrodePng} /></a>
       </h2>
     </section>
